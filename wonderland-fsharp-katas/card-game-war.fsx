1c85a6bc65e1d7cf6463e0223b3d17bea9a4af03
nestordemeure <nestordemeure@gmail.com>
2017-01-19 21:11:53 +0100

    card solved (with unit test)

--- wonderland-fsharp-katas/card-game-war.fsx
+++ wonderland-fsharp-katas/card-game-war.fsx
@@ -1,6 +1,9 @@
 // See the file card-game.md for detailed information.
 
-// feel free to use these cards or use your own data structure
+type Winner =
+    | Player1 
+    | Player2
+    | Equality
 
 type Suit =
     | Spade
@@ -15,13 +18,34 @@ type Rank =
     | King
     | Ace
 
-type Card = Suit * Rank
+type Card = Rank * Suit
 
-let playRound (card1:Card,card2:Card) =
-    failwith "not implemented: winning card"
+//-------------------------------------------------------------------------------------------------
+// SOLUTION
 
-let playGame (hand1:Card list, hand2:Card list) =
-    failwith "not implemented: game winner"
+/// returns true if the first player wins
+let playRound (card1:Card,card2:Card) = card1 > card2
+
+/// returns the identity of the payer who wins the hand
+let rec playGame (hand1:Card list, hand2:Card list) =
+    match hand1,hand2 with 
+    | [], [] -> Equality
+    | [], _ -> Player2
+    | _, [] -> Player1
+    // player 1 wins the round
+    | card1::h1, card2::h2 when playRound(card1, card2) -> 
+        let newHand1 = h1 @ [card1;card2]
+        playGame (newHand1 , h2)
+    // player 2 wins the round
+    | card1::h1, card2::h2 ->
+        let newHand2 = h2 @ [card1;card2]
+        playGame (h1 , newHand2)
+
+//-------------------------------------------------------------------------------------------------
+// TEST
+
+#r @"../packages/Unquote/lib/net45/Unquote.dll"
+open Swensen.Unquote
 
 (*
 let suits = [ Spade; Club; Diamond; Heart ]
@@ -37,20 +61,27 @@ let deck = seq {
         for rank in ranks -> suit,rank }
 *)
 
-// fill in tests for your game
 let tests () =
 
     // playRound
-    printfn "TODO: the highest rank wins the cards in the round"
-    printfn "TODO: queens are higher rank than jacks"
-    printfn "TODO: kings are higher rank than queens"
-    printfn "TODO: aces are higher rank than kings"
-    printfn "TODO: if the ranks are equal, clubs beat spades"
-    printfn "TODO: if the ranks are equal, diamonds beat clubs"
-    printfn "TODO: if the ranks are equal, hearts beat diamonds"
+    // "the highest rank wins the cards in the round"
+    test <@ playRound ( (Value 10,Club) , (Value 5,Club) ) @>
+    // "queens are higher rank than jacks"
+    test <@ playRound ( (Queen,Club) , (Jack,Club) ) @>
+    // "kings are higher rank than queens"
+    test <@ playRound ( (King,Heart) , (Queen,Heart) ) @>
+    // "aces are higher rank than kings"
+    test <@ playRound ( (Ace,Spade) , (King,Spade) ) @>
+    // "if the ranks are equal, clubs beat spades"
+    test <@ playRound ( (Value 2,Club) , (Value 2,Spade) ) @>
+    // "if the ranks are equal, diamonds beat clubs"
+    test <@ playRound ( (Value 5,Diamond) , (Value 5,Club) ) @>
+    // "if the ranks are equal, hearts beat diamonds"
+    test <@ playRound ( (Ace,Heart) , (Ace,Diamond) ) @>
 
     // playGame
-    printfn "TODO: the player loses when they run out of cards"
+    // "the player loses when they run out of cards"
+    test <@ playGame ( [(Ace,Heart)] , [] ) = Player1 @>
 
 // run the tests
 tests ()
