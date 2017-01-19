28818e3fcccaec786b3147f034f8671482e12a09
nestordemeure <nestordemeure@gmail.com>
2017-01-19 15:45:38 +0100

    magic square solved

--- wonderland-fsharp-katas/magic-square.fsx
+++ wonderland-fsharp-katas/magic-square.fsx
@@ -6,15 +6,7 @@ type Square = float[,]
 
 let dim = 3
 
-let magicSquare () =
-    // fix this so that the tests pass!
-    let square = Array2D.init dim dim (fun row col ->
-        values.[row * dim + col])
-    square
-
-
-#r @"../packages/Unquote/lib/net45/Unquote.dll"
-open Swensen.Unquote
+//-----
 
 let maxIndex = dim - 1
 let indexes = [ 0 .. maxIndex ]
@@ -34,6 +26,75 @@ let sumDownDiagonal (sq:Square) =
 let sumUpDiagonal (sq:Square) =
     [ for i in indexes -> sq.[i, maxIndex - i] ] |> List.sum
 
+//-------------------------------------------------------------------------------------------------
+
+/// unit pipe
+let (|->) x f = f x ; x
+
+let inline swap (tab: 'T []) i j = 
+    let temp = tab.[i]
+    tab.[i] <- tab.[j]
+    tab.[j] <- temp
+
+//-------------------------------------------------------------------------------------------------
+
+type FlatSquare = float []
+
+let isMagic (flatSquare : FlatSquare) =
+    let diagIncr (fs : FlatSquare) =
+        let mutable res = 0.
+        for i = 0 to dim-1 do 
+            res <- res + fs.[i + i*dim]
+        res
+    let digDecr (fs : FlatSquare) =
+        let mutable res = 0.
+        for i = 0 to dim-1 do 
+            res <- res + fs.[(dim-1-i) + i*dim]
+        res
+    let row k (fs : FlatSquare) = 
+        let mutable res = 0.
+        for i = 0 to dim-1 do 
+            res <- res + fs.[i + k*dim]
+        res
+    let col k (fs : FlatSquare) = 
+        let mutable res = 0.
+        for i = 0 to dim-1 do 
+            res <- res + fs.[k + i*dim]
+        res
+    let target = diagIncr flatSquare
+    target = digDecr flatSquare && Seq.forall ( (=) target )
+    <| seq { 
+        for k = 0 to dim-1 do 
+            yield row k flatSquare
+            yield col k flatSquare
+        }
+
+let rec allPossibleSquares startingIndex (tab : FlatSquare) = 
+    seq {
+        if startingIndex >= tab.Length-1 then
+            yield tab
+        else for i = startingIndex to tab.Length-1 do
+                swap tab i startingIndex
+                yield! allPossibleSquares (startingIndex+1) tab
+                swap tab startingIndex i
+    }
+
+let to2D (flatSquare : FlatSquare) = Array2D.init dim dim (fun row col -> flatSquare.[row * dim + col])
+
+//----
+
+let magicSquare () =
+    Array.copy values
+    |> allPossibleSquares 0
+    |> Seq.find isMagic
+    |> to2D
+    |-> printfn "%A"
+    
+
+//-------------------------------------------------------------------------------------------------
+
+#r @"../packages/Unquote/lib/net45/Unquote.dll"
+open Swensen.Unquote
 
 let tests () =
 
