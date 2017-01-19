cf01c06633e6bc5c35ecd59875feeb9192073eeb
nestordemeure <nestordemeure@gmail.com>
2017-01-19 12:20:21 +0100

    solved

--- wonderland-fsharp-katas/tiny-maze.fsx
+++ wonderland-fsharp-katas/tiny-maze.fsx
@@ -5,6 +5,7 @@ type Cell =
     | Exit
     | Empty
     | Wall
+    member this.Vide = this <> Wall
 
 type Maze = Cell [,]
 
@@ -14,8 +15,36 @@ type Path =
 
 type Solution = Path [,]
 
+/// unit pipe
+let (|->) x f = f x ; x
+
+/// bruteForce, recurcive
 let solve (maze:Maze) : Solution =
-    failwith "boom"
+    let l1 = Array2D.length1 maze
+    let l2 = Array2D.length1 maze
+    let unVisited = Array2D.create l1 l2 true
+    let result = Array2D.create l1 l2 O
+    let getNeigbours i j =
+        [
+            if i-1 >= 0 && unVisited.[i-1,j] && maze.[i-1,j].Vide then 
+                yield (i-1,j)
+            if i+1 < l1 && unVisited.[i+1,j] && maze.[i+1,j].Vide then 
+                yield (i+1,j)
+            if j-1 >= 0 && unVisited.[i,j-1] && maze.[i,j-1].Vide then 
+                yield (i,j-1)
+            if j+1 < l1 && unVisited.[i,j+1] && maze.[i,j+1].Vide then 
+                yield (i,j+1)
+        ]
+    let rec hasPath (i,j) =
+        unVisited.[i,j] <- false 
+        let onPath =
+            maze.[i,j] = Exit ||
+            match getNeigbours i j with 
+             | [] -> false
+             | neigbours -> List.exists hasPath neigbours
+        if onPath then result.[i,j] <- X ; true else false
+    if hasPath (0,0) then result |-> printfn "%A"
+    else failwith "boom"
 
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
